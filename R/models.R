28c98ea0ba8dc178c63587f204ebe926f2c24f0a
Shinichi Nakagawa <itchyshin@gmail.com>
2024-11-16 09:46:33 -0700

    Update models.R

--- R/models.R
+++ R/models.R
@@ -4,6 +4,9 @@
 
 #library(pacman)
 
+#remotes::install_github("stan-dev/cmdstanr")
+install_cmdstan(cores = 18)
+
 pacman::p_load(tidyverse, 
        ggplot2,
        brms,
@@ -12,7 +15,8 @@ pacman::p_load(tidyverse,
        loo,
        bayesplot,
        here,
-       ape
+       ape,
+       cmdstanr
 )
 
 # load data
@@ -32,9 +36,9 @@ A <- vcv.phylo(tree, corr = TRUE)
 
 # center data of interest
 
-dat$cbeak_length <- scale(dat$Beak.Length_Culmen, center = TRUE, scale = FALSE)
-dat$cbeak_width <- scale(dat$Beak.Width, center = TRUE, scale = FALSE)
-dat$cbeak_depth <- scale(dat$Beak.Depth, center = TRUE, scale = FALSE)
+dat$cbeak_length <- scale(log(dat$Beak.Length_Culmen), center = TRUE, scale = FALSE)
+dat$cbeak_width <- scale(log(dat$Beak.Width), center = TRUE, scale = FALSE)
+dat$cbeak_depth <- scale(log(dat$Beak.Depth), center = TRUE, scale = FALSE)
 
 #########################
 # location-scale model 1 using brms with phylogenetic correlation
@@ -54,16 +58,19 @@ prior1 <- default_prior(formula1,
 
 # fit model
 
+#rebuild_cmdstan()
+
 fit1 <- brm(formula1, 
             data = dat, 
             data2 = list(A = A),
             chains = 2, 
             cores = 2, 
-            iter = 4000, 
+            iter = 3000, 
             warmup = 2000,
             prior = prior1,
-            threads = threading(8)
-            #control = list(adapt_delta = 0.95, max_treedepth = 15)
+            backend = "cmdstanr",
+            threads = threading(9),
+            control = list(adapt_delta = 0.95, max_treedepth = 15)
             )
 
 summary(fit1)
@@ -72,6 +79,7 @@ summary(fit1)
 # location-scale model 2 using brms with phylogenetic correlation
 ########################
 
+# length
 formula2 <- bf(cbeak_length ~1 + (1|p|gr(Phylo, cov = A)), 
                sigma ~ 1 + (1|p|gr(Phylo, cov = A))
 )
@@ -92,14 +100,58 @@ fit2 <- brm(formula2,
             data2 = list(A = A),
             chains = 2, 
             cores = 2, 
-            iter = 4000, 
+            iter = 3000, 
             warmup = 2000,
+            backend = "cmdstanr",
             prior = prior2,
-            threads = threading(8)
-            #control = list(adapt_delta = 0.95, max_treedepth = 15)
+            threads = threading(9),
+            control = list(adapt_delta = 0.95, max_treedepth = 15)
 )
 
 summary(fit2)
+
+# width
+
+formula2b <- bf(cbeak_width ~1 + (1|p|gr(Phylo, cov = A)), 
+               sigma ~ 1 + (1|p|gr(Phylo, cov = A))
+)
+
+fit2b <- brm(formula2b, 
+            data = dat, 
+            data2 = list(A = A),
+            chains = 2, 
+            cores = 2, 
+            iter = 3000, 
+            warmup = 2000,
+            backend = "cmdstanr",
+            prior = prior2,
+            threads = threading(9),
+            control = list(adapt_delta = 0.95, max_treedepth = 15)
+)
+
+summary(fit2b)
+
+# depth
+
+formula2c <- bf(cbeak_depth ~1 + (1|p|gr(Phylo, cov = A)), 
+               sigma ~ 1 + (1|p|gr(Phylo, cov = A))
+)
+
+fit2c <- brm(formula2c, 
+            data = dat, 
+            data2 = list(A = A),
+            chains = 2, 
+            cores = 2, 
+            iter = 3000, 
+            warmup = 2000,
+            backend = "cmdstanr",
+            prior = prior2,
+            threads = threading(9),
+            control = list(adapt_delta = 0.95, max_treedepth = 15)
+)
+
+summary(fit2c)
+
 
 ########################
 # location-scale model 2 using brms with phylogenetic correlation
@@ -113,7 +165,7 @@ formula3B <- bf(cbeak_depth ~1 + (1|p|gr(Phylo, cov = A)),
                 sigma ~ 1 + (1|p|gr(Phylo, cov = A))
 )
 
-formula3 <- formula3A + formula3B
+formula3 <- formula3A + formula3B + set_rescor(TRUE) #do we need to do correlction??
 
 # creat prior
 
@@ -130,11 +182,12 @@ fit3 <- brm(formula3,
             data2 = list(A = A),
             chains = 2, 
             cores = 2, 
-            iter = 2000, 
-            warmup = 1000,
+            iter = 3000, 
+            warmup = 2000,
             prior = prior3,
-            threads = threading(9)
-            #control = list(adapt_delta = 0.95, max_treedepth = 15)
+            backend = "cmdstanr",
+            threads = threading(9),
+            control = list(adapt_delta = 0.95, max_treedepth = 15)
 )
 
 summary(fit3)
