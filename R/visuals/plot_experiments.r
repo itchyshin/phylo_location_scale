93cfaf310fdee6ab6f036f42cf5f00d4b91411dd
Szymek Drobniak <s.drobniak@unsw.edu.au>
2024-12-12 10:32:52 -0700

    plot experiments

---
+++ R/visuals/plot_experiments.r
@@ -0,0 +1,79 @@
+library(here)
+library(tidyverse)
+library(plot3D)
+library(ggplot2)
+
+
+data <- read.table(
+    here("data", "Psittaciformes", "Psittaciformes_354spp.csv"),
+    sep = ",", header = TRUE
+)
+
+data <- data %>%
+    group_by(Phylo) %>%
+    mutate(Genus = strsplit(Phylo, "_")[[1]][1]) %>%
+    ungroup()
+
+length(unique(data$Genus))
+
+data$beak_l_resid <- residuals(lm(log(Beak.Length_Culmen) ~ log(Mass), data = data))
+data$beak_w_resid <- residuals(lm(log(Beak.Width) ~ log(Mass), data = data))
+
+plot(beak_l_resid ~ log(Mass), data = data)
+
+plot(beak_l_resid ~ beak_w_resid, data = data)
+
+plot(log(Beak.Length_Culmen) ~ log(Beak.Width), data = data)
+
+ggplot(data = data, aes(
+    x = log(Beak.Length_Culmen),
+    y = log(Beak.Width), colour = Genus
+)) +
+    geom_point() +
+    theme_bw() +
+    stat_ellipse() +
+    guides(colour = FALSE)
+
+ggplot(data = data, aes(
+    x = beak_l_resid,
+    y = beak_w_resid, colour = Genus
+)) +
+    geom_point() +
+    theme_bw() +
+    stat_ellipse() +
+    guides(colour = FALSE)
+
+
+ngenera <- rev(sort(table(data$Genus)))
+ngenera <- ngenera[ngenera > 8]
+
+data <- data %>%
+    group_by(Genus) %>%
+    mutate(
+        beak_l_v = var(beak_l_resid),
+        beak_w_v = var(beak_w_resid),
+        beak_v = beak_l_v + beak_w_v
+    )
+
+data1 <- data %>%
+    filter(Genus %in% names(ngenera))
+
+ggplot(data = data1, aes(
+    x = beak_l_resid,
+    y = beak_w_resid, colour = Genus
+)) +
+    geom_point() +
+    theme_bw() +
+    stat_ellipse() +
+    guides(colour = FALSE)
+
+
+ggplot(data = data1, aes(
+    x = beak_l_resid,
+    y = beak_w_resid, colour = log(beak_v)
+)) +
+    geom_point() +
+    theme_bw() +
+    stat_ellipse(aes(group = Genus)) +
+    guides(colour = FALSE) +
+    scale_colour_gradient(low = "#0e91b9", high = "red")
